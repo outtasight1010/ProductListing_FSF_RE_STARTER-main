d7f85cf70be5a7140493c1d3d0711315ed91bea2
Michelle Lamelza <mishlamelza@gmail.com>
2023-07-16 23:15:19 -0400

    installed Product_Listing zip

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React, { useState } from 'react';
 import "./App.css";
+import data from './data/data.json';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
+  const [allProducts, setAllProducts] = useState(data)
   return (
     <div>
-      <h1>Hello, Student!</h1>
+      <NavBar/>
     </div>
   );
 }
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+import './NavBar.css';
+
+
+const NavBar = () => {
+    return (
+        <header className='text-left'>
+            <h1>Shop4Products!</h1>
+            
+        </header>
+    )
+};
+
+export default NavBar;
