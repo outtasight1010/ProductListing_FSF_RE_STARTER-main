473f5353cc4169e2dc02b5b09e477bc37af45006
Michelle Lamelza <mishlamelza@gmail.com>
2023-07-17 00:33:53 -0400

    update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import "./App.css";
 import data from './data/data.json';
 import NavBar from './Components/NavBar/NavBar';
+import ProductCard from './Components/ProductCard/ProductCard';
 //import ProductCard from './Components/ProductCard/ProductCard';
 //import ProductList from './Components/ProductList/ProductList';
 
@@ -12,6 +13,7 @@ function App() {
     <div>
       <NavBar/>
       <h2>Product catalog</h2>
+  
       
       
     
--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import data from './data.json';
+//import data from './data.json';
 import 'bootstrap/dist/css/bootstrap.css';
 
 const ProductCard = ({product}) => {
     return (
         <div className="col-md-4">
             <div className='card'>
-                <img src={product.image} className="card-img-top" alt={product.name}/>
+                <img src="https://i.dummyjson.com/data/products/1/2.jpg" className="card-img-top" />
                 <div className="card-body">
                     <h5 className="card-title">{product.name}</h5>
                     <p className="card-brand">{product.brand}</p>
