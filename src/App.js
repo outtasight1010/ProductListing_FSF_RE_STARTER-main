54d1354a6933cf95324ea3741f2646e8cc67f1aa
Michelle Lamelza <mishlamelza@gmail.com>
2023-07-19 01:24:54 -0400

    added useState function to App.js + added first Product card

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React, { useState } from 'react';
 import "./App.css";
 import data from './data/data.json';
 import NavBar from './Components/NavBar/NavBar';
-import ProductList from './Components/ProductList/ProductList';
+import ProductCard from './Components/ProductCard/ProductCard';
+
+
 
 //import ProductCard from './Components/ProductCard/ProductCard';
 //import ProductList from './Components/ProductList/ProductList';
 
-const App= () => {
+function App()  {
+  console.log(data);
+  const [allProducts, setAllProducts] = useState(data);
+  const handleButtonClick = () => {
+    setAllProducts({data}
+   );
+  };
 
-  //const [allProducts, setAllProducts] = React.useState(data);
-
-    
   return (
     
     <div>
@@ -19,10 +24,14 @@ const App= () => {
       <h2>Product catalog</h2>
       
         <hr size="5"
-        color=" black"
-        align="center"
-        width="1400px"></hr>
-      <br><br><br></br></br></br><br></br>
+        color= 'black'
+        align= 'center'
+        width="1400px">
+        </hr>
+      <br></br>
+      <ProductCard product = {allProducts}/>
+      <button onClick ={handleButtonClick}>Update Product</button>
+      
       
       
     </div>
--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,20 +1,23 @@
 import React from "react";
+import './ProductCard.css';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
 const ProductCard = ({product}) => {
     return (
-        <div className="col-md-4">
+        <div className="col-md-2">
             <div className='card'>
-                <img src="https://i.dummyjson.com/data/products/1/2.jpg"  className="card-img-top" alt="Apple Iphone 9" />
-                <div className="card-body">
-                    <h5 className="card-title">{product.name}</h5>
-                    <p className="card-brand">{product.brand}</p>
-                    <p className="card-category">{product.category}</p>
-                    <p className="card-stock">{product.stock}</p>
-                    <p className="card-rating">{product.rating}</p>
-                    <button type="submit">Add to cart</button>
+                <div className="card-1">
+                    <img src="https://i.dummyjson.com/data/products/1/2.jpg"  className="card-img-top" alt="Apple Iphone 9" />
+                    <div className="card-body">
+                        <h5 className="card-title">Iphone 9</h5>
+                        <p className="card-brand">Apple</p>
+                        <p className="card-category">smartphones</p>
+                        <p className="card-stock">94</p>
+                        <p className="card-rating">4.96 stars</p>
+                        <button type="submit" >Add to cart</button>
 
+                    </div>
                 </div>
             </div>
         </div>
