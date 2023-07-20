5bf891a9c64d07397a53bcf44daeccd2c426d473
Michelle Lamelza <mishlamelza@gmail.com>
2023-07-20 04:30:09 -0400

    cont. working on Cards format

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -10,41 +10,100 @@ const ProductCard = ({product}) => {
     
  
     return (
-        <div className="container">
-            <div className="card">
-                <div className="imgBx">
-                <img src ={product.image} alt =""/>
-                </div>
-                <div className="content">
-                    <div className="productName">
-                        <h3>{product.title}</h3>
+        <div className="page">
+            <div className="row">
+                <div className="col-sm">
+                    <div className="card card-cascade card-ecommerce wider shadow mb-5">
+                        <div className="view view-cascade overlay text-center">
+                        <img class="card-img-top" src={product.image}
+            alt=""></img>
+                         <a>
+                            <div className="mask rgba-white-slight"></div>
+                         </a>
+                        </div>
+                        <div className="card-body card-body-cascade text-center">
+                            <h4 className="card-title"><strong><a href = "">{product.title}</a></strong></h4>
+                            <p className="card-description">{product.description}</p>
+                            <p className="card-price">${product.price}</p>
+                            <p className="card-brand">{product.brand}</p>
+                            <p className="card-stock">{product.stock}</p>
+                            <p className="card-category">{product.category}</p>
+
+                            <ul className="row-rating">
+                                <li><i className="fa fa-star">{product.rating}</i></li>
+                                <li><i className="fa fa-star"></i></li>
+                                <li><i className="fa fa-star"></i></li>
+                                <li><i className="fa fa-star"></i></li>
+                                <li><i className="fa fa-star"></i></li>
+                            </ul>
+                            <div className="card-footer">
+                                <button>Add to cart</button>
+                            </div>
+
+                        </div>
+                        </div>
                     </div>
-                    <div className="price-rating">
-                        <h2>${product.price}</h2>
-                    </div>
-                    <div className="rating">
-                        <i className="fa fa-star" >{product.rating}</i>
-                        
-                    </div>
-                    <div className="product description">
-                        <p>{product.description}</p>
-                    </div>
-                    <div className="product-stock">
-                        <p>{product.stock}</p>
-                    </div>
-                    <div className="product-brand">
-                        <p>{product.brand}</p>
-                    </div>
-                    <div className="product-category">
-                        <p>{product.category}</p>
-                    </div>
-                    <button>Add to cart</button>
                 </div>
 
-            </div> 
+            </div>
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //<div className="container">
+            //<div className="card">
+                //<div className="imgBx">
+                //<img src ={product.image} alt =""/>
+                //</div>
+                //<div className="content">
+                    //<div className="productName">
+                       // <h3>{product.title}</h3>
+                    //</div>
+                   // <div className="price-rating">
+                        //<h2>${product.price}</h2>
+                    //</div>
+                    //<div className="rating">
+                        //<i className="fa fa-star" >{product.rating}</i>
+                        //<i className="fa fa-star" ></i>
+                        //<i className="fa fa-star" ></i>
+                        //<i className="fa fa-star" ></i>
+                        //<i className="fa fa-star grey" ></i>
+                    //</div>
+                    //<div className="product description">
+                       // <p>{product.description}</p>
+                    //</div>
+                    //<div className="product-stock">
+                        //<p>{product.stock}</p>
+                    //</div>
+                    //<div className="product-brand">
+                        //<p>{product.brand}</p>
+                    //</div>
+                    //<div className="product-category">
+                        //<p>{product.category}</p>
+                    //</div>
+                    //<button>Add to cart</button>
+                //</div>
+
+            //</div> 
             
                
-        </div>
+        //</div>
         
         
      
