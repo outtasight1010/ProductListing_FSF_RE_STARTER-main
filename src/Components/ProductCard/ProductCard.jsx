852d24563f284b92d8bb4223be3226fe99057c50
Michelle Lamelza <mishlamelza@gmail.com>
2023-07-20 02:01:45 -0400

    updated ProductCard classes(card-class)

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -10,17 +10,41 @@ const ProductCard = ({product}) => {
     
  
     return (
-        <div className="product-card">
-            <img src ={product.image} alt =""/>
-            <h3>{product.title}</h3>
-            <p>{product.description}</p>
-            <p>${product.price}</p>
-            <p>{product.rating}</p>
-            <p>{product.stock}</p>
-            <p>{product.brand}</p>
-            <p>{product.category}</p>
-            <button>Add to cart</button>
-          
+        <div className="container">
+            <div className="card">
+                <div className="imgBx">
+                <img src ={product.image} alt =""/>
+                </div>
+                <div className="content">
+                    <div className="productName">
+                        <h3>{product.title}</h3>
+                    </div>
+                    <div className="price-rating">
+                        <h2>${product.price}</h2>
+                    </div>
+                    <div className="rating">
+                        <i className="fa fa-star" >{product.rating}</i>
+                        <i className="fa fa-star" ></i>
+                        <i className="fa fa-star" ></i>
+                        <i className="fa fa-star" ></i>
+                        <i className="fa fa-star" ></i>
+                    </div>
+                    <div className="product description">
+                        <p>{product.description}</p>
+                    </div>
+                    <div className="product-stock">
+                        <p>{product.stock}</p>
+                    </div>
+                    <div className="product-brand">
+                        <p>{product.brand}</p>
+                    </div>
+                    <div className="product-category">
+                        <p>{product.category}</p>
+                    </div>
+                    <button>Add to cart</button>
+                </div>
+
+            </div> 
             
                
         </div>
