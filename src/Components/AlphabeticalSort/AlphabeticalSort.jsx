d5f7a5d566af664d258172dc81da5b1ce19450f3
Michelle Lamelza <mishlamelza@gmail.com>
2023-08-17 01:54:34 -0400

    experimenting with Alphabetical component of products

--- src/Components/AlphabeticalSort/AlphabeticalSort.jsx
+++ src/Components/AlphabeticalSort/AlphabeticalSort.jsx
@@ -1,16 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
+import products from '../ProductCard/products';
 
 const AlphabeticalSort = ({ onSortChange }) => {
-  const [selectedSort, setSelectedSort] = useState('name-asc'); 
-
   const handleSortChange = (event) => {
-    setSelectedSort(event.target.value);
-    onSortChange(event.target.value); //  I will then pass the selected sorting option back to the parent component
+    onSortChange(event.target.value);
   };
 
   return (
     <div>
-      <select value={selectedSort} onChange={handleSortChange}>
+      <select onChange={handleSortChange}>
         <option value="name-asc">Sort by Name (A to Z)</option>
         <option value="name-desc">Sort by Name (Z to A)</option>
       </select>
@@ -19,3 +17,4 @@ const AlphabeticalSort = ({ onSortChange }) => {
 };
 
 export default AlphabeticalSort;
+
--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -44,66 +44,6 @@ const ProductCard = ({product}) => {
                         </div>
                         </div>
                     </div>
-               
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //<div className="container">
-            //<div className="card">
-                //<div className="imgBx">
-                //<img src ={product.image} alt =""/>
-                //</div>
-                //<div className="content">
-                    //<div className="productName">
-                       // <h3>{product.title}</h3>
-                    //</div>
-                   // <div className="price-rating">
-                        //<h2>${product.price}</h2>
-                    //</div>
-                    //<div className="rating">
-                        //<i className="fa fa-star" >{product.rating}</i>
-                        //<i className="fa fa-star" ></i>
-                        //<i className="fa fa-star" ></i>
-                        //<i className="fa fa-star" ></i>
-                        //<i className="fa fa-star grey" ></i>
-                    //</div>
-                    //<div className="product description">
-                       // <p>{product.description}</p>
-                    //</div>
-                    //<div className="product-stock">
-                        //<p>{product.stock}</p>
-                    //</div>
-                    //<div className="product-brand">
-                        //<p>{product.brand}</p>
-                    //</div>
-                    //<div className="product-category">
-                        //<p>{product.category}</p>
-                    //</div>
-                    //<button>Add to cart</button>
-                //</div>
-
-            //</div> 
-            
-               
-        //</div>
-        
         
      
     );
